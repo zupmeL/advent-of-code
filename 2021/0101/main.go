4e940459b25157a717c30810f8b78907a5b111d0
Stefano Prudenzi <84486+zupmeL@users.noreply.github.com>
2021-12-02 15:57:09 +0100

    2021 - Complete day 1

---
+++ 2021/0101/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	firstItem := getFirstItem( scanner )
+	increments := getIncrements( scanner, firstItem )
+
+	fmt.Println("INCREMENTS: ", increments)
+}
+
+func getFirstItem( scanner *bufio.Scanner) int {
+	scanner.Scan()
+	item, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		log.Fatal("Couldn't convert scanned row to number")
+	}
+	return item
+}
+
+func getIncrements( scanner *bufio.Scanner, previousItem int) int {
+	if scanner.Scan() {
+		item, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal("Couldn't convert scanned row to number")
+		}
+		if previousItem < item {
+			return getIncrements( scanner, item ) + 1
+		} else {
+			return getIncrements( scanner, item )
+		}
+	}
+	return 0
+}
---
+++ 2021/0102/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+const SLICE_LENGTH = 3
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	arr := getAllItems(scanner)
+
+	if len(arr) > SLICE_LENGTH {
+		firstSum := getSliceSum( arr[0:SLICE_LENGTH])
+		increments := getIncrements( arr[1:], firstSum )
+		fmt.Println("INCREMENTS", increments)
+	} else {
+		log.Fatal("Not enough items")
+	}
+
+}
+
+func getAllItems(scanner *bufio.Scanner) []int {
+	items := make([]int, 0)
+
+	for scanner.Scan() {
+		item, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal("Couldn't convert scanned row to number")
+		}
+		items = append(items, item)
+	}
+	return items
+}
+
+func getSliceSum(slice []int) int {
+	sum := 0
+	for _, value := range slice {
+		sum += value
+	}
+	return sum
+}
+
+func getIncrements(arr []int, previousSum int) int {
+	if len(arr) >= SLICE_LENGTH {
+		sum := getSliceSum(arr[0:SLICE_LENGTH])
+		if sum > previousSum {
+			return getIncrements(arr[1:], sum) + 1
+		} else {
+			return getIncrements(arr[1:], sum)
+		}
+	}
+	return 0
+}
